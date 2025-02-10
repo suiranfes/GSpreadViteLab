ede1882b2973b7bf47afe8caa475eb4986be047f
mint73 <ny.snoopy.1164@gmail.com>
2025-02-10 10:27:34 +0900

    feat: add Google Sheets sign-in and sheet addition functionality

--- src/utils/gss.tsx
+++ src/utils/gss.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { gapi } from 'gapi-script';
 
 const GoogleSheetsComponent: React.FC = () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   useEffect(() => {
     const initClient = () => {
       gapi.client.init({
@@ -9,6 +11,15 @@ const GoogleSheetsComponent: React.FC = () => {
         clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
         scope: 'https://www.googleapis.com/auth/spreadsheets',
         discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+      }).then(() => {
+        const authInstance = gapi.auth2.getAuthInstance();
+        if (authInstance.isSignedIn.get()) {
+          setIsSignedIn(true);
+        } else {
+          authInstance.signIn().then(() => {
+            setIsSignedIn(true);
+          });
+        }
       });
     };
 
@@ -16,26 +27,51 @@ const GoogleSheetsComponent: React.FC = () => {
   }, []);
 
   const handleSignIn = () => {
-    gapi.auth2.getAuthInstance().signIn().then(() => {
-      const params = {
-        spreadsheetId: import.meta.env.VITE_GOOGLE_SPREADSHEET_ID,
-        range: '1!A1',
-        valueInputOption: 'RAW',
-        resource: {
-          values: [['Hello', 'World']],
-        },
-      };
+    if (!isSignedIn) return;
 
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      gapi.client.sheets.spreadsheets.values.update(params).then((response: any) => {
-        console.log('Data updated:', response);
-      });
+    const params = {
+      spreadsheetId: import.meta.env.VITE_GOOGLE_SPREADSHEET_ID,
+      range: '1!A1',
+      valueInputOption: 'RAW',
+      resource: {
+        values: [['Hello', 'World']],
+      },
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    gapi.client.sheets.spreadsheets.values.update(params).then((response: any) => {
+      console.log('Data updated:', response);
+    });
+  };
+
+  const handleAddSheet = () => {
+    if (!isSignedIn) return;
+
+    const params = {
+      spreadsheetId: import.meta.env.VITE_GOOGLE_SPREADSHEET_ID,
+      resource: {
+        requests: [
+          {
+            addSheet: {
+              properties: {
+                title: 'UMA',
+              },
+            },
+          },
+        ],
+      },
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    gapi.client.sheets.spreadsheets.batchUpdate(params).then((response: any) => {
+      console.log('Sheet added:', response);
     });
   };
 
   return (
     <div>
       <button onClick={handleSignIn}>Sign In and Update Sheet</button>
+      <button onClick={handleAddSheet}>Add New Sheet</button>
     </div>
   );
 };
